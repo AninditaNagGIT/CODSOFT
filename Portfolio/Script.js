3be7dd6c331b20f5531e959054abc29dd5f37ae0
AninditaNagGIT <144224114+AninditaNagGIT@users.noreply.github.com>
2023-09-19 11:15:01 +0530

    Add files via upload

---
+++ Portfolio/Script.js
@@ -0,0 +1,17 @@
+﻿function Calculate(val) {
+    var res = document.getElementById('txtResult');
+    res.value += val;
+}
+function Result() {
+    var num1 = document.getElementById('txtResult').value;
+    var num2 = eval(num1);
+    document.getElementById('txtResult').value = num2;
+}
+function Clear() {
+    var inp = document.getElementById('txtResult');
+    inp.value = '';
+}
+function Back() {
+    var back = document.getElementById('txtResult');
+    back.value = back.value.slice(0, -1);
+}
